36413179e791b514f2b65773036e0519e7bdb071
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-09 13:58:11 +0300

    Create run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+"""
+   Module for running the app
+"""
+from flask import Flask
+from api.routes.urls import Urls
+APP = Flask(__name__)
+Urls.fetch_urls(APP)
+if __name__ == '__main__':
+    APP.run()
+    
