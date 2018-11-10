03645a45b64cd2cb96a3ea4369da489c91aadc64
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-10 16:55:42 +0300

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from flask import Flask
 from api.routes.urls import Urls
 
 if __name__ == '__main__':
-    Urls.fetch_urls(APP)
+    Urls.fetch_urls(app)
     port = int(os.environ.get('PORT', 5000))
 
     if port == 5000:
