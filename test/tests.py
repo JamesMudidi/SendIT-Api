23a2bad50ea9d428a2957e1a342e00c8ede67195
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-10 20:45:28 +0300

    Create tests.py

---
+++ test/tests.py
@@ -0,0 +1,26 @@
+accepted_data = {
+          "parcel_name": "Books",
+          "description": "On the way",
+          "pickup": "Bugolobi",
+          "destination": "Kireka"
+           }
+  
+invalid_data = {
+          "parcel_name": "Books",
+          "description": 10000,
+          "pickup": "Bugolobi",
+          "destination": "Kireka"
+          }
+  
+blank_fields = {
+          "parcel_name": "",
+          "description": "",
+          "pickup": "",
+          "destination": "Kireka"
+          }
+blank_space = {
+          "parcel_name": "  ",
+          "description": "On the way",
+          "pickup": "Bugolobi",
+          "destination": "Kireka"
+          }
