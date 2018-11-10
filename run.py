71ae6d6d029284c643430c511cb1019019ec37e4
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-10 19:42:42 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,10 @@
-"""
-   Module for running the app
-"""
-import os
 from flask import Flask
 from api.routes.urls import Urls
-
+app = Flask(__name__)
+Urls.fetch_urls(app)
 if __name__ == '__main__':
-    Urls.fetch_urls(app)
-    port = int(os.environ.get('PORT', 5000))
-
-    if port == 5000:
+   
+   if port == 5000:
         app.debug = True
-
-    app.run(host='127.0.0.1', port=port)
+   
+   app.run(host='127.0.0.1', port=port)
