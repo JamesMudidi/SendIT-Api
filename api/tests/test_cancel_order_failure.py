376689374f44f145d7efd12440733dccae7a23ec
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-12 00:18:48 +0300

    Update test_cancel_order_failure.py

--- api/tests/test_cancel_order_failure.py
+++ api/tests/test_cancel_order_failure.py
@@ -6,33 +6,34 @@ from api.tests.create_order import CreateOrder
 class TestNoCancelOrder(TestCase):
 
     def test_cancel_order_failure(self):
+
         Model.lsts.clear()
         Model.userLst.clear()
 
         CreateOrder().create_order({
-                'user_id':1,
-                'pickup': 'kampala',
-                'destination': 'kireka',
-                'description': 'fragile',
-                'weight': 10,
-                'product': 'parcel',
-            })
+            'user_id':1,
+            'pickup': 'kampala',
+            'destination': 'kireka',
+            'description': 'fragile',
+            'weight': 10,
+            'product': 'parcel',
+        })
         CreateOrder().create_order({
-                'user_id':2,
-                'pickup': 'kampala',
-                'destination': 'kololo',
-                'description': 'fragile',
-                'weight': 20,
-                'product': 'parcel',
-            })
+            'user_id':2,
+            'pickup': 'kampala',
+            'destination': 'kololo',
+            'description': 'fragile',
+            'weight': 20,
+            'product': 'parcel',
+        })
         CreateOrder().create_order({
-                'user_id':3,
-                'pickup': 'kamwokya',
-                'destination': 'mengo',
-                'description': 'fragile',
-                'weight': 30,
-                'product': 'parcel',
-            })
+            'user_id':3,
+            'pickup': 'kamwokya',
+            'destination': 'mengo',
+            'description': 'fragile',
+            'weight': 30,
+            'product': 'parcel',
+        })
 
         req = CreateOrder().client().put('/api/v1/parcels/10/cancel/')
         resp = json.loads(req.data.decode())
