fe8043daa81935ad5c260e1d0890d12729f3240b
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-11 23:41:08 +0300

    Update test_create_order.py

---
+++ api/tests/test_create_order.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from flask import json
+from api.tests.create_order import CreateOrder
+
+class TestCreateProduct(TestCase):
+
+    def test_create_order(self):
+
+        order = {
+            'user_id':1,
+            'pickup': 'kampala',
+            'destination': 'kireka',
+            'description': 'fragile',
+            'weight': 10,
+            'product': 'parcel',
+        }
+        post = CreateOrder().create_order(order)
+
+        resp = json.loads(post.data.decode())
+        self.assertEqual(resp['success'], True)
+        self.assertEqual(resp['data']['product'], 'parcel')
+        self.assertEqual(post.content_type, 'application/json')
+        self.assertEqual(post.status_code, 201)
