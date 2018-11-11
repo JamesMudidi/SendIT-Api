85f237e4b48bd15ab093937c80aef82462a25c23
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-11 23:31:03 +0300

    Update model.py

---
+++ api/models/model.py
@@ -0,0 +1,51 @@
+from flask import jsonify
+
+class Model:
+    """
+    Add data
+    """
+    lsts = []
+    userLst = []
+
+    def get_order(self, order_id=None):
+        """
+        Get data
+        """
+        if order_id is None:
+            return jsonify({"success":True, "data":Model.lsts}), 200
+        for order in Model.lsts:
+            if order.get('order_id') == order_id:
+                return jsonify({"success":True, "data":order}), 200
+        return jsonify({"success":False, "error":{"message": "Order not found"}}), 404
+
+    def get_order_user(self, user_id):
+        """
+        Get data posted by a specific user
+        """
+        for order in Model.lsts:
+            if order.get('user_id') == user_id:
+                Model.userLst.append(order)
+            else:
+                Model.userLst.clear()
+
+        if len(Model.userLst) > 0:
+            return jsonify({"success":True, "data":Model.userLst}), 200
+        return jsonify({"success":False, "error":{"message": "Order not found"}}), 404
+
+
+    def cancel_order(self, order_id):
+        """
+        Cancel order
+        """
+        for order in Model.lsts:
+            if order.get('order_id') == order_id:
+                order['status'] = 'cancelled'
+                return jsonify({"success":True, "data":order}), 200
+        return jsonify({"success":False, "error":{"message": "Order canceled"}}), 404
+
+    def create_order(self, order):
+        """
+        Add an order
+        """
+        Model.lsts.append(order)
+        return jsonify({"success":True, "data":order}), 201
