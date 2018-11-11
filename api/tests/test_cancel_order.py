8c031e6aa9460e37e0b757a56e7503b693e14fd4
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-11 23:36:39 +0300

    test_cancel_order.py

---
+++ api/tests/test_cancel_order.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+from flask import json
+from api.models.model import Model
+from api.tests.create_order import CreateOrder
+
+class TestCancelOrder(TestCase):
+    """
+    Canceling orders test results
+    """
+    def test_cancel_order(self):
+        Model.lsts.clear()
+        Model.userLst.clear()
+
+        CreateOrder().create_order({
+            'user_id':1,
+            'pickup': 'kampala',
+            'destination': 'kireka',
+            'description': 'fragile',
+            'weight': 50,
+            'product': 'parcel',
+        })
+        CreateOrder().create_order({
+            'user_id':2,
+            'pickup': 'kampala',
+            'destination': 'kololo',
+            'description': 'fragile',
+            'weight': 10,
+            'product': 'parcel',
+        })
+        CreateOrder().create_order({
+            'user_id':3,
+            'pickup': 'kamwokya',
+            'destination': 'mengo',
+            'description': 'fragile',
+            'weight': 1,
+            'product': 'parcel',
+        })
+
+        req = CreateOrder().client().put('/api/v1/parcels/1/cancel/')
+        resp = json.loads(req.data.decode())
+        self.assertEqual(resp['success'], True)
+        self.assertEqual(resp['data']['status'], 'cancelled')
+        self.assertEqual(req.status_code, 200)
