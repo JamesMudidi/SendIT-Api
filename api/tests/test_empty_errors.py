352ce8efca85bd228e5a16e49b8c581acad97c51
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-11 23:42:57 +0300

    Update test_empty_errors.py

---
+++ api/tests/test_empty_errors.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from flask import json
+from api.tests.create_order import CreateOrder
+
+class TestEmptyErrors(TestCase):
+
+    def test_empty_errors(self):
+        """
+        Test blank spaces
+        """
+        order = {
+           'user_id':1,
+            'pickup': 'kampala',
+            'destination': 'kireka',
+            'description': 'fragile',
+            'weight': 10,
+            'product': 'parcel',
+        }
+        post = CreateOrder().create_order(order)
+
+        resp = json.loads(post.data.decode())
+        self.assertEqual(resp['success'], False)
+        self.assertEqual(resp['error']['message'], 'No blank spaces allowed')
+        self.assertEqual(post.content_type, 'application/json')
+        self.assertEqual(post.status_code, 400)
