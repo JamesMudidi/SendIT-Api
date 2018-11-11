a20fcb3fd80b9f2dd36c2eb94e4c380052345073
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-11 23:59:11 +0300

    Update test_zero_errors.py

---
+++ api/tests/test_zero_errors.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from flask import json
+from api.tests.create_order import CreateOrder
+
+class TestZeroErrors(TestCase):
+
+    def test_zero_errors(self):
+        order = {
+            'user_id':1,
+            'pickup': 'kampala',
+            'destination': 'kireka',
+            'description': 'fragile',
+            'weight': 10,
+            'product': 'parcel',
+        }
+        post = CreateOrder().create_order(order)
+
+        resp = json.loads(post.data.decode())
+        self.assertEqual(resp['success'], False)
+        self.assertEqual(resp['error']['message'], 'User_id and Weight should not be less than zero')
+        self.assertEqual(post.content_type, 'application/json')
+        self.assertEqual(post.status_code, 400)
