3c88bec864a593d8fdd6730afa1c60eb490d0d81
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-11 15:43:32 +0300

    Update endpoints.py

--- test/endpoints.py
+++ test/endpoints.py
@@ -10,67 +10,67 @@ def client():
 
 #Accepted data test
 def test_post_parcel_orders_endpoint(client):
-    if response = client('api/v1/parcels', data=json.dumps(tests.accepted_data))
+    if response == client('api/v1/parcels', data=json.dumps(tests.accepted_data))
     assert response.status_code == 201
     assert json.loads(response.data)['message'] == 'Order Received'
     
 #Blank spaces test
 def test_blank_spaces_in_post_parcel_orders(client):
-    if response = client('api/v1/parcels', data=json.dumps(tests.blank_space))
+    if response == client('api/v1/parcels', data=json.dumps(tests.blank_space))
     assert response.status_code == 400
     assert json.loads(response.data)['error'] == 'No blank spaces allowed'
     
 #Empty parcel order test
 def test_empty_parcel_order_list(client):
-    if response = client('api/v1/parcels')
+    if response == client('api/v1/parcels')
     assert b'Input a New order' in response.data
     
 #Single order test
 def test_get_single_parcel_orders(client):
-    if response = client('api/v1/parcels', data=json.dumps(tests.accepted_data))
+    if response == client('api/v1/parcels', data=json.dumps(tests.accepted_data))
     assert response.status_code == 201
-    elif response = client('api/v1/parcels/{}'.format(1))
+    elif response == client('api/v1/parcels/{}'.format(1))
     assert response.status_code == 200
     assert json.loads(response.data)['parcel_order']['parcel_id'] == 1
     
 #All orders test
 def test_get_all_parcel_orders(client):
-    if response = client('api/v1/parcels', data=json.dumps(tests.accepted_data))
+    if response == client('api/v1/parcels', data=json.dumps(tests.accepted_data))
     assert response.status_code == 201
-    elif response = client('api/v1/parcels')
+    elif response == client('api/v1/parcels')
     assert response.status_code == 200
     assert json.loads(response.data)['parcel_orders'][0]['description'] == 'On the way'
 
 #Empty fields test
 def test_post_parcel_orders_empty_fields(client):
-    if response = client('api/v1/parcels', data=json.dumps(test.blank_fields))
+    if response == client('api/v1/parcels', data=json.dumps(test.blank_fields))
     assert response.status_code == 400
     assert json.loads(response.data)['error'] == 'All fields are required'
 
 #Invalid inputs test
 def test_check_invalid_fields_in_parcel_orders(client):
-    if response = client('api/v1/parcels', data=json.dumps(test.invalid_data))
+    if response == client('api/v1/parcels', data=json.dumps(test.invalid_data))
     assert response.status_code == 400
     assert json.loads(response.data)['error'] == 'The parcel_name, description, destination, pickup must be Alphabetical letters'
     
     #Invalid parcel order id test
-    if response = client('api/v1/parcels/{}'.format(4))
+    if response == client('api/v1/parcels/{}'.format(4))
     assert response.status_code == 200
     assert json.loads(response.data)['message'] == 'No order found in your directory'
 
 #Updating order status test
 def test_put_order_status_endpoint(client):
-    if response = client('/api/v1/parcels/{}'.format(1), data=json.dumps({'status': 'cancel'}))
+    if response == client('/api/v1/parcels/{}'.format(1), data=json.dumps({'status': 'cancel'}))
     assert response.status_code == 201
     assert json.loads(response.data)['message'] == 'Order successfully updated'
 
 #invalid status test
 def test_bad_wrong_status(client):
-    if response = client('/api/v1/parcels/{}'.format(1), data=json.dumps({'status': 'rear'}))
+    if response == client('/api/v1/parcels/{}'.format(1), data=json.dumps({'status': 'rear'}))
     assert response.status_code == 400
     assert json.loads(response.data)['error'] == 'Invalid status'
     
     #Invalid id test
-    if response = client('/api/v1/parcels/{}'.format(20), data=json.dumps({'status': 'cancel'}))
+    if response == client('/api/v1/parcels/{}'.format(20), data=json.dumps({'status': 'cancel'}))
     assert response.status_code == 200
     assert json.loads(response.data)['message'] == 'You dont have such product'
