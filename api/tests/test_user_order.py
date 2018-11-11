796d204cda2eea2bf599895d920440156ba64c97
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-12 00:41:56 +0300

    Update test_user_order.py

--- api/tests/test_user_order.py
+++ api/tests/test_user_order.py
@@ -13,7 +13,7 @@ class TestUserOrder(TestCase):
             'user_id':1,
             'pickup': 'kampala',
             'destination': 'kireka',
-            'description': 'fragile',
+            'description': 'light',
             'weight': 10,
             'product': 'parcel',
         })
@@ -21,9 +21,9 @@ class TestUserOrder(TestCase):
             'user_id':1,
             'pickup': 'kampala',
             'destination': 'kololo',
-            'description': 'fragile',
+            'description': 'heavy',
             'weight': 20,
-            'product': 'parcel',
+            'product': 'box',
         })
         CreateOrder().create_order({
             'user_id':2,
@@ -31,11 +31,11 @@ class TestUserOrder(TestCase):
             'destination': 'mengo',
             'description': 'fragile',
             'weight': 30,
-            'product': 'Parcels',
+            'product': 'crate',
         })
 
         req = CreateOrder().client().get('/api/v1/users/2/parcels/')
         resp = json.loads(req.data.decode())
         self.assertEqual(resp['success'], True)
-        self.assertEqual(resp['data'][0]['product'], 'Parcels')
+        self.assertEqual(resp['data'][0]['product'], 'crate')
         self.assertEqual(req.status_code, 200)
