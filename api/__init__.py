1c5a9607f3de5b255acd3f5e6a0a775b3e08faed
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-11 23:24:00 +0300

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,1 @@
-from flask import Flask
-from config import config
 
-def create_app(config_name):
-    """Create app instance"""
-    app = Flask(__name__)
-    app.config.from_object(config[config_name])
-    # registering the blue print object
-    from api.api_v1 import api_v1
-    app.register_blueprint(api_v1, url_prefix='/api/v1')
-    return app
