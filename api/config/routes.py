33ddf2be5bc0714e0db4ad43ae29c064bdaa8fa6
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-15 20:34:32 +0300

    Update routes.py

--- api/config/routes.py
+++ api/config/routes.py
@@ -1,22 +1,18 @@
 from api.controllers.controller import Controller
 
 class Routes:
-    """
-    Routes class
-    """
+    ### Routes class ###
 
     @staticmethod
     def fetch_routes(app):
-        """
-        static method for fetching all routes
-        """
+        ### static route ###
 
         #default route
         @app.route('/')
         def index():
             return "<h1>Welcome to SendIT</h1><p>You Request | We Deliver</p>"
 
-        #parcels endpoints
+        ### parcels endpoints ###
         app.add_url_rule(
             '/parcels/',
             view_func=Controller.as_view('createParcel'),
@@ -24,25 +20,25 @@ class Routes:
             strict_slashes=False
         )
         app.add_url_rule(
-            '/api/v1/parcels/',
+            '/parcels/',
             view_func=Controller.as_view('getParcels'),
             methods=['GET'],
             strict_slashes=False
         )
         app.add_url_rule(
-            '/api/v1/parcels/<int:parcel_id>',
+            '/parcels/<int:parcel_id>',
             view_func=Controller.as_view('getSingleParcel'),
             methods=['GET'],
             strict_slashes=False
         )
         app.add_url_rule(
-            '/api/v1/users/<int:user_id>/parcels',
+            '/users/<int:user_id>/parcels',
             view_func=Controller.as_view('getParcelByUser'),
             methods=['GET'],
             strict_slashes=False
         )
         app.add_url_rule(
-            '/api/v1/parcels/<int:parcel_id>/cancel',
+            '/parcels/<int:parcel_id>/cancel',
             view_func=Controller.as_view('cancelParcel'),
             methods=['PUT'],
             strict_slashes=False
