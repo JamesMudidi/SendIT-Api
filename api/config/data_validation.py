568cbd1bff09284e8e4636533b56d9e6cb5782db
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-16 19:00:10 +0300

    Update and rename validation.py to data_validation.py

--- api/config/data_validation.py
+++ api/config/data_validation.py
@@ -1,17 +1,25 @@
-### Module to return validations values ###
+"""
+Module to return validations values
+"""
 from flask import jsonify
 
-class validation:
-    ### Class with methods to return validation values ###
+class DataValidation:
+    """
+    Class with methods to return validation values
+    """
 
     error_msg = {"success":False, "error":{"message":""}}
 
     def __init__(self, req):
-        ### Class constructor for intiating a request object ###
+        """
+        Class constructor for intiating a request object
+        """
         self.req = req
 
-    def string(self):
-        ### Method that checks for string errors ###
+    def string_error(self):
+        """
+        Method that checks for string errorss
+        """
         if isinstance(self.req['pickup'], str) and isinstance(self.req['destination'], str):
             if isinstance(self.req['description'], str) and isinstance(self.req['product'], str):
                 return True
@@ -19,24 +27,30 @@ class validation:
         DataValidation.error_msg['error']['message'] = msg
         return jsonify(DataValidation.error_msg), 400
 
-    def integer(self):
-        ### Integer errors ###
+    def integer_error(self):
+        """
+        Integer errors
+        """
         if isinstance(self.req['user_id'], int) and isinstance(self.req['weight'], int):
             return True
         msg = 'User_id and Weight must be integers'
         DataValidation.error_msg['error']['message'] = msg
         return jsonify(DataValidation.error_msg), 400
 
-    def blank(self):
-        ### Blank ###
+    def empty_keys(self):
+        """
+        Blank
+        """
         if self.req['user_id'] != '' and self.req['description'] != '' and self.req['product'] != '' and self.req['description'] != '' and self.req['pickup'] != '' and self.req['weight'] != '':
             return True
         msg = 'Blanks not allowed'
         DataValidation.error_msg['error']['message'] = msg
         return jsonify(DataValidation.error_msg), 400
 
-    def zero(self):
-        ### Check user_id or weight is less than zero ###
+    def lesser_zero(self):
+        """
+        Check user_id or weight is less than zero
+        """
         if self.req['user_id'] > 0 and self.req['weight'] > 0:
             return True
         msg = 'user_id and weight should be greater than zero'
@@ -44,7 +58,9 @@ class validation:
         return jsonify(DataValidation.error_msg), 400
 
     def validate(self):
-        ### Check  validity for posted data ###
+        """
+        Check  validity for posted data
+        """
         if isinstance(self.string_error(), bool) and isinstance(self.integer_error(), bool):
             if isinstance(self.empty_keys(), bool) and isinstance(self.lesser_zero(), bool):
                 return True
