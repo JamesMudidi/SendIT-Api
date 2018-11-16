aed0d18d8a9d93fee06bbf8cb3141f4c1f2b226c
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-16 18:59:27 +0300

    Update routes.py

--- api/config/routes.py
+++ api/config/routes.py
@@ -1,45 +1,49 @@
 from api.controllers.controller import Controller
 
-class routes:
-    ### Routes class ###
+class Routes:
+    """
+    Routes class
+    """
 
     @staticmethod
     def fetch_routes(app):
-        ### static route ###
+        """
+        static method for fetching all routes
+        """
 
         #default route
         @app.route('/')
         def index():
-            return "<h1>Welcome to SendIT</h1><p>You Request | We Deliver</p>"
+            return "<h1>Welcome to SendIT</h1>"
 
-        ### parcels endpoints ###
+        #parcels endpoints
         app.add_url_rule(
-            '/parcels/create/',
-            view_func=Controller.as_view('createOrder'),
+            '/api/v1/parcels/',
+            view_func=Controller.as_view('createParcel'),
             methods=['POST'],
             strict_slashes=False
         )
         app.add_url_rule(
-            '/parcels/get/',
-            view_func=Controller.as_view('createParcel'),
+            '/api/v1/parcels/',
+            view_func=Controller.as_view('getParcels'),
             methods=['GET'],
             strict_slashes=False
         )
         app.add_url_rule(
-            '/parcels/get/<int:parcel_id>/',
-            view_func=Controller.as_view('singleOrder'),
+            '/api/v1/parcels/<int:parcel_id>',
+            view_func=Controller.as_view('getSingleParcel'),
             methods=['GET'],
             strict_slashes=False
         )
         app.add_url_rule(
-            '/users/parcels/<int:user_id>/',
-            view_func=Controller.as_view('UserOrder'),
+            '/api/v1/users/<int:user_id>/parcels',
+            view_func=Controller.as_view('getParcelByUser'),
             methods=['GET'],
             strict_slashes=False
         )
         app.add_url_rule(
-            '/parcels/cancel/<int:parcel_id>/',
-            view_func=Controller.as_view('cancelOrder'),
+            '/api/v1/parcels/<int:parcel_id>/cancel',
+            view_func=Controller.as_view('cancelParcel'),
             methods=['PUT'],
             strict_slashes=False
         )
