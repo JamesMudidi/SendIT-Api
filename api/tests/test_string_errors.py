6d415d56602b9c4112dc59f4f8ee8083a6a79223
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-16 18:55:31 +0300

    Update test_string_errors.py

--- api/tests/test_string_errors.py
+++ api/tests/test_string_errors.py
@@ -15,7 +15,6 @@ class TestStringErrors(TestCase):
         }
         post = CreateOrder().create_order(order)
 
-        resp = json.loads(post.data.decode())
         self.assertEqual(resp['success'], False)
         self.assertEqual(resp['error']['message'], 'Pickup, Destination, Description and Product should be Strings')
         self.assertEqual(post.content_type, 'application/json')
